cbfedeaba2f11a84841b2e43214410410ed67aca
miok-jung <miok.dev@gmail.com>
2024-05-04 16:26:23 +0900

    test: CKEditor5 classic editor Test

---
+++ src/components/CKEditor5.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>TEST CKEDITOR5</h1>
+    <ckeditor
+      :editor="ClassicEditor"
+      v-model="editorData"
+      :config="editorConfig"
+    ></ckeditor>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+const editorData = ref<string>('<p>Content of the editor.</p>');
+const editorConfig = {
+  //
+};
+</script>
+
+<style scoped></style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,7 +6,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
-
+  {
+    path: '/test',
+    component: () => import('layouts/TestLayout.vue'),
+    children: [{ path: '', component: () => import('pages/TestPage.vue') }],
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
