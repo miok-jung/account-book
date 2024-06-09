ea7ff62ca7ac5c106d47d9258e5e716b4c908bb5
miok-jung <miok.dev@gmail.com>
2024-06-09 23:17:20 +0900

    update: i18n 메인 수정 업데이트

--- src/i18n/ja/index.ts
+++ src/i18n/ja/index.ts
@@ -1,7 +1,14 @@
 export default {
+  main: {
+    title: '題目',
+    lightMode: 'ライトモード',
+    darkMode: '暗いモード',
+    changeTo: '$CONTENT$切り替え',
+    category: 'カテゴリー',
+    modeDescriptionGuide:
+      '言語選択を通じてページ翻訳を確認することができます。',
+  },
   accountBook: {
     title: '家計簿',
   },
-  lightMode: 'ライトモード',
-  darkMode: '暗いモード',
 };
