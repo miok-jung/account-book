fa78a6b8527548b53d583c199d00e80ea9dd5431
miok-jung <miok.dev@gmail.com>
2024-07-03 21:06:50 +0900

    feat: mixins 여백 및 가로 사이즈 규칙 추가

--- quasar.config.js
+++ quasar.config.js
@@ -22,7 +22,7 @@ module.exports = configure(function (/* ctx */) {
     boot: ['i18n', 'axios', 'ckeditor'],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
-    css: ['app.scss'],
+    css: ['app.scss', '_mixins.scss', 'styles.scss'],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
