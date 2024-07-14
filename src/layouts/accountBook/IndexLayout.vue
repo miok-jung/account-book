488b751c3449d0055abe9382a409e9371d9a8c1a
miok-jung <miok.dev@gmail.com>
2024-07-14 09:19:06 +0900

    feat: Add to Index Vue

---
+++ src/layouts/accountBook/IndexLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-layout
+    view="lHh lpr lFf"
+    container
+    style="height: 100vh"
+    class="shadow-2 rounded-borders"
+  >
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/pages/accountBook/IndexPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="row flex-center" style="height: 100vh; border: 1px solid red">
+    <p v-for="n in 30" :key="n">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis velit
+      nulla, sit perspiciatis odio repellat magni distinctio quaerat at in
+      necessitatibus perferendis. Fugiat asperiores debitis tempore ex
+      distinctio saepe consequuntur.
+    </p>
+    <br />
+    <br />
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
